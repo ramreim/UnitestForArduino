585496805351422066bae041b049376bce310e1a
remreim <studen_studen@yahoo.com>
2024-07-30 12:45:58 +0300

    digital write test created

--- UnitTestForArduino/UnitTestForArduino/UnitTestForArduino.cpp
+++ UnitTestForArduino/UnitTestForArduino/UnitTestForArduino.cpp
@@ -54,5 +54,38 @@ namespace UnitTestForArduino
 			Assert::AreEqual(digitalSw05Read.pinNumber, 5);
 			Assert::AreEqual(digitalSw05Read.functionResult, 6);
 		}
+
+		TEST_METHOD(DigitalWrite_sw_10_hw_12)
+		{
+			ExecuteSw sw;
+
+			ExecuteHw hw;
+
+			ExecuterGeneral executerGener;
+
+			Result digitalSw10WriteHIGH = executerGener.DigitalWriteCmdd(sw, 10, 1);
+
+			Assert::AreEqual(digitalSw10WriteHIGH.functionNr, 2);
+			Assert::AreEqual(digitalSw10WriteHIGH.pinNumber, 10);
+			Assert::AreEqual(digitalSw10WriteHIGH.functionResult, 1);
+
+			Result digitalSw10WriteLOW = executerGener.DigitalWriteCmdd(sw, 10, 0);
+
+			Assert::AreEqual(digitalSw10WriteLOW.functionNr, 2);
+			Assert::AreEqual(digitalSw10WriteLOW.pinNumber, 10);
+			Assert::AreEqual(digitalSw10WriteLOW.functionResult, 0);
+
+			Result digitalHw11WriteHIGH = executerGener.DigitalWriteCmdd(hw, 11, 1);
+
+			Assert::AreEqual(digitalHw11WriteHIGH.functionNr, 2);
+			Assert::AreEqual(digitalHw11WriteHIGH.pinNumber, 11);
+			Assert::AreEqual(digitalHw11WriteHIGH.functionResult, 14);
+
+			Result digitalHw111WriteLOW = executerGener.DigitalWriteCmdd(hw, 11, 0);
+
+			Assert::AreEqual(digitalHw111WriteLOW.functionNr, 2);
+			Assert::AreEqual(digitalHw111WriteLOW.pinNumber, 11);
+			Assert::AreEqual(digitalHw111WriteLOW.functionResult, 13);
+		}
 	};
 }
