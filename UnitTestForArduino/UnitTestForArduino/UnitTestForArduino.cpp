52fba439eb565389e4243c32613fbe3b737dea3a
remreim <studen_studen@yahoo.com>
2024-07-30 12:49:36 +0300

    digital write test created

--- UnitTestForArduino/UnitTestForArduino/UnitTestForArduino.cpp
+++ UnitTestForArduino/UnitTestForArduino/UnitTestForArduino.cpp
@@ -12,27 +12,6 @@ namespace UnitTestForArduino
 	TEST_CLASS(UnitTestForArduino)
 	{
 	public:
-		
-		TEST_METHOD(AnalogRead_sw_02_hw_04)
-		{
-			ExecuteSw sw;
-
-			ExecuteHw hw;
-
-			ExecuterGeneral executerGener;
-
-			Result analogSw02 = executerGener.AnalogReadCmdd(sw, 2);
-
-			Assert::AreEqual(analogSw02.functionNr, 3);
-			Assert::AreEqual(analogSw02.pinNumber, 2);
-			Assert::AreEqual(analogSw02.functionResult, 5);
-
-			Result analogHw02 = executerGener.AnalogReadCmdd(hw, 4);
-
-			Assert::AreEqual(analogHw02.functionNr, 3);
-			Assert::AreEqual(analogHw02.pinNumber, 4);
-			Assert::AreEqual(analogHw02.functionResult, 7);
-		}
 
 		TEST_METHOD(DigitalRead_sw_03_hw_05)
 		{
@@ -86,6 +65,36 @@ namespace UnitTestForArduino
 			Assert::AreEqual(digitalHw111WriteLOW.functionNr, 2);
 			Assert::AreEqual(digitalHw111WriteLOW.pinNumber, 11);
 			Assert::AreEqual(digitalHw111WriteLOW.functionResult, 13);
+		}
+
+		TEST_METHOD(AnalogRead_sw_02_hw_04)
+		{
+			ExecuteSw sw;
+
+			ExecuteHw hw;
+
+			ExecuterGeneral executerGener;
+
+			Result analogSw02 = executerGener.AnalogReadCmdd(sw, 2);
+
+			Assert::AreEqual(analogSw02.functionNr, 3);
+			Assert::AreEqual(analogSw02.pinNumber, 2);
+			Assert::AreEqual(analogSw02.functionResult, 5);
+
+			Result analogHw02 = executerGener.AnalogReadCmdd(hw, 4);
+
+			Assert::AreEqual(analogHw02.functionNr, 3);
+			Assert::AreEqual(analogHw02.pinNumber, 4);
+			Assert::AreEqual(analogHw02.functionResult, 7);
+		}
+
+		TEST_METHOD(AnalogWrite_sw_03_hw_05)
+		{
+			ExecuteSw sw;
+
+			ExecuteHw hw;
+
+			ExecuterGeneral executerGener;
 		}
 	};
 }
